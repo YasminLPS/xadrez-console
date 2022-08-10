86a2067f305df78e69b28cb7de6a6c0091aeff09
Yasmin Lopes <yasminqalopes54@gmail.com>
2022-08-10 17:46:35 -0300

    Melhorando a classe Tela

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -13,8 +13,10 @@ namespace xadrez_console
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Tower(tab, Color.Preta), new Position(0, 0));
-                tab.colocarPeca(new Tower(tab, Color.Preta), new Position(1, 9));
+                tab.colocarPeca(new Tower(tab, Color.Preta), new Position(1, 3));
                 tab.colocarPeca(new King(tab, Color.Preta), new Position(2, 4));
+
+                tab.colocarPeca(new Tower(tab, Color.Branca), new Position(3, 5));
 
                 Tela.imprimirTabuleiro(tab);
             }
